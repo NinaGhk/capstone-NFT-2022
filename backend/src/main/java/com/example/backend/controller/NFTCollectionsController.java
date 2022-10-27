fe0b715a8ee717a689745c3d6b65d2a65eb8deb8
Nina Guo <ninaguo1305@gmail.com>
2022-10-27 16:29:48 +0800

    BE: add CollectionsService ,Controller und model

---
+++ backend/src/main/java/com/example/backend/controller/NFTCollectionsController.java
@@ -0,0 +1,35 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.MetadataFromCollection;
+import com.example.backend.model.NFTCollectionsResponseElement;
+import com.example.backend.service.CollectionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api.nftport.xyz/v0/")
+public class NFTCollectionsController {
+
+    private CollectionsService collectionsService;
+
+    @Autowired
+    public NFTCollectionsController(CollectionsService service){
+        this.collectionsService= service;
+    }
+
+    @GetMapping("")
+    public MetadataFromCollection getAllNfts(){
+        return collectionsService.getAllNfts();
+    }
+
+    @GetMapping(path="{query}")
+    public List<NFTCollectionsResponseElement> getNftsByQuery(@PathVariable String query){
+        return collectionsService.getNftsByQuery(query);
+    }
+
+}
---
+++ backend/src/main/java/com/example/backend/service/CollectionsService.java
@@ -0,0 +1,45 @@
+package com.example.backend.service;
+
+
+import com.example.backend.model.MetadataFromCollection;
+import com.example.backend.model.NFTCollectionsResponseElement;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Service
+public class CollectionsService {
+
+    private final WebClient client = WebClient.create("https://api.nftport.xyz/v0/");
+
+    private MetadataFromCollection AllNfts() {return AllNfts();}
+
+
+
+     public List<NFTCollectionsResponseElement> getNftsByQuery(String query){
+         NFTCollectionsResponseElement responseElement =
+                 client.get()
+                         .uri("nfts?chain=ethereum&include=metadata&Authorization=d4f5003c-bf80-4a38-8b65-444e985d8919" + query)
+                         .retrieve()
+                         .toEntity(NFTCollectionsResponseElement.class)
+                         .block()
+                         .getBody();
+         return responseElement.getResult();
+     }
+
+    public MetadataFromCollection getAllNfts() {
+        MetadataFromCollection AllNfts =
+                client.get()
+                        .uri("nfts?chain=ethereum&include=metadata&Authorization=d4f5003c-bf80-4a38-8b65-444e985d8919")
+                        .retrieve()
+                        .toEntity(MetadataFromCollection.class)
+                        .block()
+                        .getBody();
+        return AllNfts();
+
+    }
+}
+
+
+
