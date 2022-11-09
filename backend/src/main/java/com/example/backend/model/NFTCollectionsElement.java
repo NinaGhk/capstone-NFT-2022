b93955ff15a6c61c0de579f02093c29150b8d3c6
Nina Guo <ninaguo1305@gmail.com>
2022-11-09 18:59:54 +0800

    BE:fixed NFT CollectionsService,Controller

--- backend/src/main/java/com/example/backend/model/NFTCollectionsElement.java
+++ backend/src/main/java/com/example/backend/model/NFTCollectionsElement.java
@@ -1,12 +1,16 @@
 package com.example.backend.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Document ("collections")
 
 public class NFTCollectionsElement {
 
