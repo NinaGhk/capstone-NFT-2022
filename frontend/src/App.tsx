b0fa19d681a2aa4185aaa5f27f98670b0ab49b0b
Nina Guo <ninaguo1305@gmail.com>
2022-11-12 01:19:23 +0800

    FE: fixed pages (App,Favorite,collection,Home)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {HashRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AppPage from "./pages/AppPage"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import useFavoriteNfts from "./hooks/useFavoriteNfts";
+import FavoritePage from "./pages/FavoritePage";
+import useCollectionNft from "./hooks/useCollectionNft";
+import CollectionPage from "./pages/CollectionPage";
 
-export default App;
+
+
+
+export default function App() {
+
+    const{getAllNfts,favoriteNfts,deleteNft,favoriteNft}=useFavoriteNfts();
+    const {collectionNfts} =useCollectionNft();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className={"App-title"}> NFT World </h1>
+
+                <HashRouter>
+                    <Routes>
+                        <Route path="/" element={<AppPage/>}/>
+                        <Route path ="/homepage" element = {<HomePage favoriteNft={favoriteNft}  />}/>
+                        <Route path ="/favorite" element ={<FavoritePage favoriteNfts={favoriteNfts} getAllFavorite={getAllNfts} deleteNft={deleteNft} markHeartAsFavorite={getAllNfts}/>}/>
+                        <Route path = "/collection" element = {<CollectionPage collectionNfts={collectionNfts} getAllCollection={getAllNfts}/>}/>
+                        </Routes>
+                </HashRouter>
+
+        </header>
+
+        </div>
+    );
+};
+
+
