2f5e0cb047e7d01be882770710bf03b6b735a2d5
Nina Guo <ninaguo1305@gmail.com>
2022-11-14 23:47:29 +0800

    fixed:  CSS + header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header-container">
+            <h1>NFT Watchers</h1>
+            <p>
+                <small>The only place to watch NFT online</small>
+            </p>
+        </div>
+    );
+}
+
+export default Header;
